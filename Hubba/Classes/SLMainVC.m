0bda543672ab2394c7ab96ce210b4efb5f4baf7f
Raheel Ahmad <rahmad@fitbit.com>
2013-03-09 18:17:40 -0800

    MainVC updates UI using authentication helpers

--- Hubba/Classes/SLMainVC.m
+++ Hubba/Classes/SLMainVC.m
@@ -7,13 +7,15 @@
 //
 
 #import "SLMainVC.h"
-#import "SLOAuth2Client.h"
+#import "SLAPIClient.h"
 
 @interface SLMainVC ()
 
 - (IBAction)initiateLogin:(id)sender;
 
 @property (nonatomic, strong) IBOutlet UIWebView *authWebView;
+@property (strong, nonatomic) IBOutlet UIBarButtonItem *loginButton;
+@property (strong, nonatomic) SLAPIClient *APIClient;
 
 @end
 
@@ -24,7 +26,34 @@
 
 - (IBAction)initiateLogin:(id)sender {
 	[self.view addSubview:self.authWebView];
-	[[SLOAuth2Client sharedClientWithAPIName:@"Github"] initiateAuthorizationWithWebView:self.authWebView];
+	NSLog(@"Authenticated: %d", self.APIClient.authenticated);
+	[self.APIClient initiateAuthorizationWithWebView:self.authWebView onCompletion:^(BOOL success) {
+		if (success) {
+			NSLog(@"Yay! success");
+			NSLog(@"Authenticated: %d", self.APIClient.authenticated);
+		} else {
+			NSLog(@"FAIL!!!");
+		}
+		[self updateUI];
+	}];
+}
+
+- (IBAction)	logout:(id)sender {
+	[self.APIClient resetAuthentication];
+	[self updateUI];
+}
+
+#pragma mark - UI
+
+- (void)updateUI {
+	if (self.APIClient.authenticated) {
+		self.loginButton.enabled = NO;
+		[self.loginButton setTitle:NSLocalizedString(@"Authenticated", nil)];
+		[self.authWebView removeFromSuperview];
+	} else {
+		self.loginButton.action = @selector(initiateLogin:);
+		[self.loginButton setTitle:NSLocalizedString(@"Log In", nil)];
+	}
 }
 
 #pragma mark - View Controller
@@ -41,8 +70,12 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	self.authWebView.frame = CGRectMake(10, 40, CGRectGetWidth(self.view.frame) - 20, CGRectGetHeight(self.view.frame) - 80);
+	self.authWebView.frame = CGRectMake(10, 48, CGRectGetWidth(self.view.frame) - 20, CGRectGetHeight(self.view.frame) - 80);
 	self.authWebView.layer.borderWidth = 1.0f;
+	self.APIClient = [SLAPIClient sharedClientWithAPIName:@"Github" baseURL:@"https://api.github.com"];
+	self.loginButton.target = self;
+	
+	[self updateUI];
 }
 
 - (void)didReceiveMemoryWarning
