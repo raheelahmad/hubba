f02fb3985079312adcd0c01af63eb304621d4ab6
Raheel Ahmad <rahmad@fitbit.com>
2013-03-16 16:22:56 -0700

    SLUser

---
+++ Hubba/Classes/SLUser.h
@@ -0,0 +1,22 @@
+//
+//  SLUser.h
+//  Hubba
+//
+//  Created by Raheel Ahmad on 3/16/13.
+//  Copyright (c) 2013 Sakun Labs. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+#import "SLRemoteManagedObject.h"
+
+@interface SLUser : SLRemoteManagedObject
+
+@property (nonatomic, retain) NSString * name;
+@property (nonatomic, retain) NSString * login;
+@property (nonatomic, retain) NSNumber * remoteID;
+@property (nonatomic, retain) NSString * company;
+@property (nonatomic, retain) NSString * email;
+@property (nonatomic, retain) NSSet *ownedRepositories;
+
+@end
---
+++ Hubba/Classes/SLUser.m
@@ -0,0 +1,51 @@
+//
+//  SLUser.m
+//  Hubba
+//
+//  Created by Raheel Ahmad on 3/16/13.
+//  Copyright (c) 2013 Sakun Labs. All rights reserved.
+//
+
+#import "SLUser.h"
+
+
+@implementation SLUser
+
+@dynamic name;
+@dynamic login;
+@dynamic remoteID;
+@dynamic company;
+@dynamic email;
+@dynamic ownedRepositories;
+
++ (NSString *)endPoint {
+	return @"/user";
+}
+
++ (NSArray *)sortDescriptors {
+	return @[ [NSSortDescriptor sortDescriptorWithKey:@"remoteID" ascending:YES] ];
+}
+
++ (NSString *)pathToObject {
+	return nil;
+}
+
++ (BOOL)appearsAsCollection {
+	return NO;
+}
+
++ (NSPredicate *)localPredicateForRemoteObject:(NSDictionary *)remoteObject {
+	return [NSPredicate predicateWithFormat:@"remoteID == %@", [remoteObject valueForKey:@"id"]];
+}
+
++ (NSDictionary *)remoteToLocalMappings {
+	return @{
+			   @"id"			: @"remoteID",
+			   @"name"			: @"name",
+			   @"login"			: @"login",
+			   @"company"		: @"company",
+			   @"email"			: @"email",
+	  };
+}
+
+@end
